a0740758593fc115efbdfe6ebdfb6b767e503e86
Devanand <dev.ryder88@gmail.com>
2019-08-03 05:24:41 +0530

    fix:Fix the build error

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,36 +5,43 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 // import "./layout.css"
 import '../styles/app.scss';
-import '../scripts/app.js';
+// import '../scripts/app.js';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+class Layout extends Component {
+	constructor(props) {
+		super(props);
+		// this.state = { ready: false };
+	}
 
-  return (
-    <>
-      <div>
-        <main>{children}</main>
-        <footer className="uk-text-center uk-padding uk-section-muted">
-          © {new Date().getFullYear()}, Corner Project
-        </footer>
-      </div>
-    </>
-  )
+	componentDidMount = () => {
+		// state = { ready: false };
+		if (typeof document !== 'undefined') {
+			const uikit = require('uikit');
+			const icons = require('uikit/dist/js/uikit-icons.min');
+			uikit.use(icons);
+			// this.setState({ ready: true });
+		}
+	};
+
+	render() {
+		return (
+	      <>
+	        <div>
+	          <main>{this.props.children}</main>
+	          <footer className="uk-text-center uk-padding uk-section-muted">
+	            © {new Date().getFullYear()}, Corner Project
+	          </footer>
+	        </div>
+	      </>
+	    )
+	}
 }
 
 Layout.propTypes = {
